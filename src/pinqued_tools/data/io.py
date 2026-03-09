6b40365d165f591261a1f873d79b14ae632744cf
Michael Vorobiov <mvorobiov@wm.edu>
2026-03-09 19:51:51 -0400

    input-output functions file

---
+++ src/pinqued_tools/data/io.py
@@ -0,0 +1,26 @@
+'''
+Docstring for pinqued_tools.data.io
+
+The class handles reading data from disk.
+'''
+
+
+def read_frames(file: str):
+    '''
+    Reads raw fluorescence images from an h5 file.
+    The files are HDF5 and have extensions either '.h5' or '.hdf5'.
+
+    :param file: Path to the file to be loaded. 
+    :type file: str
+    '''
+    pass
+
+def read_squids(file: str):
+    '''
+    Reads processed squid maps generated from fluorescence images.
+    The files are HDF5 and have extensions either '.h5' or '.hdf5'.
+    
+    :param file: Path to the file to be loaded
+    :type file: str
+    '''
+    pass
