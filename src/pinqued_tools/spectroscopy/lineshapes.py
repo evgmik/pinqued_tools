3f95ea02c910f83f2bab9c80323392b8491e20f3
Eugeniy E. Mikhailov <evgmik@gmail.com>
2026-05-24 12:24:03 -0400

    added some extra attributes for Stark Map

--- src/pinqued_tools/spectroscopy/lineshapes.py
+++ src/pinqued_tools/spectroscopy/lineshapes.py
@@ -203,7 +203,14 @@ class StarkMap():
         self._approx_poly2ndOrder = Poly(np.polyfit(self._Efield[-3:], self._freq[-3:], 2))  # we can always make parabola on 3 points
         self._minEfild_for_poly2ndOderValidity = self._Efield[-3]
         self._approx_highOrder = None
+
+        self._max_shift_indx = self._freq.argmax()
+        self._max_shift_freq = self._freq[self._max_shift_indx]
+        self._max_shift_Efield = self._Efield[self._max_shift_indx]
+
         self.monotonic = np.all(np.diff(self._freq) <= 0)  # is stark shift monotonically decreasing
+        if not self.monotonic:
+            print("The Stark shift are not monotonic, will split in two monotonic regions")
 
         # let's try to extend the range where 2nd degree polynomial still fits
         Np = len(self._Efield) 
